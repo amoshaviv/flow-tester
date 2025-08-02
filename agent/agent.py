6366f0663a38347b776e37a3e9e8f61d1cbd17d0
Amos Haviv <mail@amoshaviv.com>
2025-08-02 12:46:47 +0200

    Adding S3 Save

--- agent/agent.py
+++ agent/agent.py
@@ -12,19 +12,6 @@ browser = Browser(
     )
 )
 
-def base64_to_image(base64_string: str, output_filename: str):
-    """Convert base64 string to image."""
-    import base64
-    import os
-
-    if not os.path.exists(os.path.dirname(output_filename)):
-        os.makedirs(os.path.dirname(output_filename))
-
-    img_data = base64.b64decode(base64_string)
-    with open(output_filename, "wb") as f:
-        f.write(img_data)
-    return output_filename
-
 async def processTask(task):
     agent = Agent(
         task=task,
@@ -33,22 +20,4 @@ async def processTask(task):
     )
     result = await agent.run()
     return result
-    # print(result)
-    # screenshots = result.screenshots()
-    # number_screenshots = 0
-    # for next_screenshot in screenshots:
-    #     number_screenshots=number_screenshots+1
-    #     path = f"./screenshots/{number_screenshots}.png"
-    #     img_path = base64_to_image(
-    #         base64_string=str(next_screenshot),
-    #         output_filename=path
-    #     )
-    #     print(img_path)
 
-def upload_file_to_s3(file_path, bucket_name, s3_key):
-    s3 = client('s3')
-    try:
-        s3.upload_file(file_path, bucket_name, s3_key)
-        print(f"Uploaded {file_path} to s3://{bucket_name}/{s3_key}")
-    except Exception as e:
-        print(f"Failed to upload {file_path} to S3: {e}")
--- agent/consumer.py
+++ agent/consumer.py
@@ -3,21 +3,46 @@ import asyncio
 import time
 from agent import processTask
 import json
+import base64
 
-
+s3 = client('s3')
 
 # Config
+S3_BUCKET_NAME='flow-tester'
 QUEUE_URL = 'https://sqs.us-west-2.amazonaws.com/746664778706/flow-tester-test-runs-queue'
 REGION = 'us-west-2'
 
+
 # Initialize SQS client
 sqs = client('sqs', region_name=REGION)
+
+
+def base64_to_s3_image(base64_string: str, path: str):
+    img_data = base64.b64decode(base64_string)
+    s3.put_object(
+        Bucket=S3_BUCKET_NAME,
+        Key=path,
+        Body=img_data,
+        ContentType='image/png',
+    )
+
+def save_result(result):
+    screenshots = result.screenshots()
+    number_screenshots = 0
+    for next_screenshot in screenshots:
+        number_screenshots=number_screenshots+1
+        path = f"/screenshots/{number_screenshots}.png"
+        base64_to_s3_image(
+            base64_string=str(next_screenshot),
+            path=path
+        )
 
 def process_message(body):
     """Simulate a task, replace this with real logic."""
     print(f"Processing message: {body}")
     message = json.loads(body)
-    asyncio.run(processTask(message['task']))
+    result = asyncio.run(processTask(message['task']))
+    save_result(result)
     print("Task complete.")
 
 def worker():
